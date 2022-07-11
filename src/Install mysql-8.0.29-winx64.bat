fab52efff71e7c2df5a6b44c8989d4caedadb882
thiagorigonatti <25761610+thiagorigonatti@users.noreply.github.com>
2022-07-11 09:11:38 -0300

    Add unicode support; open MySQL automatically after install it

--- src/Install mysql-8.0.29-winx64.bat
+++ src/Install mysql-8.0.29-winx64.bat
@@ -1,12 +1,11 @@
 @echo off
+chcp 65001
 
-
-setlocal enabledelayedexpansion
 cd /d %~dp0
 
 net session >nul 2>&1
     if %errorLevel% == 0 (
-        echo Instalando MySQL8.0 - Script criado por: Thiago Rigonatti.
+        echo Instalando MySQL-8.0.29... AGUARDE
     ) else (
         echo Falha: É necessário ter permissões de administrador, execute como administrador.
 		pause
@@ -14,7 +13,6 @@ net session >nul 2>&1
     )
 
 Call :UnZipFile "C:\MySQL\" "%~dp0mysql-8.0.29-winx64.zip"
-
 
 :UnZipFile <ExtractTo> <newzipfile>
 set vbs="%temp%\_.vbs"
@@ -38,33 +36,33 @@ copy %~dp0my.ini C:\MySQL\MySQL-8.0.29\my.ini
 if not exist C:\MySQL\MySQL-8.0.29\uploads mkdir C:\MySQL\MySQL-8.0.29\uploads
 
 setlocal enabledelayedexpansion
+
 set serviceName=MySQL80
 
 sc query %serviceName%
 cls
 
-echo Instalando MySQL8.0 - Script criado por: Thiago Rigonatti.
-
 IF ERRORLEVEL 1060 (
-    C:\MySQL\MySQL-8.0.29\bin\mysqld.exe --install %serviceName% --defaults-file=C:\MySQL\MySQL-8.0.29\my.ini
+    C:\MySQL\MySQL-8.0.29\bin\mysqld --install %serviceName% --defaults-file=C:\MySQL\MySQL-8.0.29\my.ini
 ) else (
 	echo "Já existe um serviço nomeado %serviceName% instalado, por favor digite um nome para este novo serviço."
-	set /p service_name=Nome do serviço: 
+	set /p service_name=Nome do serviço:
 	set serviceName=!service_name!
-	C:\MySQL\MySQL-8.0.29\bin\mysqld.exe --install !service_name! --defaults-file=C:\MySQL\MySQL-8.0.29\my.ini
+	C:\MySQL\MySQL-8.0.29\bin\mysqld --install !service_name! --defaults-file=C:\MySQL\MySQL-8.0.29\my.ini
 )
 
 sc config %serviceName% start=demand
 
-echo Extraindo data...
-
+echo Extraindo arquivos... AGUARDE
 echo net start %serviceName% > "%homedrive%%homepath%\Desktop\iniciar %serviceName%.bat"
 echo C:\MySQL\MySQL-8.0.29\bin\mysql.exe -u root >> "%homedrive%%homepath%\Desktop\iniciar %serviceName%.bat"
-
 echo net stop %serviceName% > "%homedrive%%homepath%\Desktop\parar %serviceName%.bat"
 
 C:\MySQL\MySQL-8.0.29\bin\mysqld --initialize-insecure
 
 net start %serviceName%
 
-exit
+C:\MySQL\MySQL-8.0.29\bin\mysql -u root
+
+cls
+C:\Windows\system32\cmd
